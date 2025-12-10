d9ebf8f10b96edb1284cf1d2ff3428b91511c1be
x907 <github-actions[bot]@users.noreply.github.com>
2025-12-10 05:05:12 -0600

    Hardcode Meta Pixel ID for reliable deployment

--- components/analytics/meta-pixel.tsx
+++ components/analytics/meta-pixel.tsx
@@ -2,28 +2,36 @@
 
 import Script from "next/script";
 
-// Meta Pixel for conversion tracking
+// Meta Pixel for conversion tracking - ID: 832779702979195
 export function MetaPixel() {
-  const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
-
-  if (!PIXEL_ID) {
-    return null;
-  }
+  // Hardcoded to ensure it's included in the build
+  const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "832779702979195";
 
   return (
-    <Script id="meta-pixel" strategy="afterInteractive">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${PIXEL_ID}');
-        fbq('track', 'PageView');
-      `}
-    </Script>
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
   );
 }
